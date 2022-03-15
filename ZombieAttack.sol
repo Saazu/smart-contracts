891d7f5af9e4ff4a1c63e36b573e0885099f46b1
Alfred Delle <alfreddelle@Alfreds-MacBook-Pro.local>
2022-03-15 14:31:35 +0000

    Add zombie attack contract

---
+++ ZombieAttack.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+    uint256 randNonce = 0;
+
+    function randMod(uint256 _modulus) internal returns (uint256) {
+        randNonce++;
+        return
+            uint256(keccak256(abi.encodePacked(now, msg.sender, randNonce))) %
+            _modulus;
+    }
+}
