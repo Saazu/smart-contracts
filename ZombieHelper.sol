a78cae16d9b8b26f1afa6fef231054c0f1848d84
Alfred Delle <alfreddelle@Alfreds-MacBook-Pro.local>
2022-03-15 13:53:01 +0000

    Add payable functionn allow zombies to level up

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -5,9 +5,16 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./ZombieFeeding";
 
 contract ZombieHelper is ZombieFeeding {
+    uint levelUpFee = 0.001 ether;
+
     modifier aboveLevel(uint256 _level, uint256 _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
+    }
+
+    function levelUp(uint _zombieId) external payable {
+      require(msg.value === levelUpFee);
+      zombies[_zombieId].level++;
     }
 
     function changeName(uint256 _zombieId, string calldata _newName)
