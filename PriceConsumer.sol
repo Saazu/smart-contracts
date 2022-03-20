6d73fd788ff81d42f7eb64e734bb50f972edb1a4
Alfred Delle <alfreddelle@Alfreds-MacBook-Pro.local>
2022-03-20 02:28:12 +0000

    Use On Chain VRF to generate random dna

---
+++ PriceConsumer.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.7;
+
+//matches solidity version
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface public priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function getDecimals() public view returns (uint8) {
+        uint8 decimals = priceFeed.decimals();
+        return decimals;
+    }
+}
--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -7,19 +7,20 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./Ownable.sol";
 import "./SafeMath.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-using SafeMath for uint256;
+contract ZombieFactory is Ownable, VRFConsumerBase {
+    using SafeMath for uint256;
 
-contract ZombieFactory is Ownable {
     uint256 zombieDigit = 16;
     uint256 dnaModulus = 10**16;
-    uint cooldownTime = 1 days;
+    uint256 cooldownTime = 1 days;
 
     event ZombieCreated(uint256 zombieId, string name, uint256 dna);
 
     struct Zombie {
         string name;
-        uint dna;
+        uint256 dna;
         uint32 level;
         uint32 readyTime;
         uint16 winCount;
@@ -31,15 +32,31 @@ contract ZombieFactory is Ownable {
     mapping(uint256 => address) public zombieToOwner;
     mapping(address => uint256) ownerZombieCount;
 
-    function _generateRandomDna(string memory _str) private returns (uint256) {
-        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
+    constructor()
+        public
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token
+        )
+    {
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 100000000000000000;
+    }
+
+    function _createZombie(string memory _name, uint256 _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+    function getRandomNumber() public returns (bytes32 requestId) {
+        return requestRandomness(keyHash, fee);
     }
 
     function _createZombie(string memory _name, uint256 _dna) internal {
         //minus 1 because function returns length of array and we want to use id
         //for array indexing
-        uint256 id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+        uint256 id = zombies.push(
+            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
+        ) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit ZombieCreated(id, _name, _dna);
@@ -47,7 +64,8 @@ contract ZombieFactory is Ownable {
 
     function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
-        uint256 randDna = _generateRandomDna(_name);
+        uint256 randomNumber = uint256(getRandomNumber());
+        uint256 randDna = randomNumber % dnaModulus;
         createZombie(_name, randDna);
     }
 }
